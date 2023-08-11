ad4c7663bce430cf28e189cebc26e407665657e0
kiritoxkiriko <wangxinmeng1997@hotmail.com>
2023-08-11 12:39:37 +0800

    Update docker-image-tranfer.sh

--- docker-image-tranfer.sh
+++ docker-image-tranfer.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# MIT License
+#
+# Copyright (c) 2023 kiritoxkiriko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # Initialize our own variables
 pull_registry=""
 push_registry=""
@@ -7,14 +29,16 @@ images=""
 
 # Display help function
 display_help() {
-    echo "Usage: $0 [options...]" >&2
+    echo "Usage: dit [options...]" >&2
     echo
-    echo "   -p, --pull-registry    The registry to pull the images from."
-    echo "   -u, --push-registry    The registry to push the images to."
-    echo "   -i, --images           A comma-separated list of images to transfer."
+    echo "   -p, --pull-registry    The registry to pull the images from. (Optional)"
+    echo "   -u, --push-registry    The registry to push the images to. (Required)"
+    echo "   -i, --images           A comma-separated list of images to transfer. (Required)"
     echo
-    echo "Example: $0 -p mypullregistry -u mypushregistry -i image1,image2,image3"
-    echo "Example: $0 --pull-registry mypullregistry --push-registry mypushregistry --images image1,image2,image3"
+    echo "Example: dit -p mypullregistry -u mypushregistry -i image1,image2,image3"
+    echo "Example: dit --pull-registry mypullregistry --push-registry mypushregistry --images image1,image2,image3"
+    echo "Example: dit -u mypushregistry -i image1,image2,image3"
+    echo "Example: dit --push-registry mypushregistry --images image1,image2,image3"
     exit 1
 }
 
@@ -52,7 +76,7 @@ while (( "$#" )); do
 done
 
 # Check if all required arguments are provided
-if [ -z "$pull_registry" ] || [ -z "$push_registry" ] || [ -z "$images" ]; then
+if [ -z "$push_registry" ] || [ -z "$images" ]; then
     echo "Error: Not all required arguments are provided."
     display_help
 fi
@@ -62,11 +86,18 @@ IFS=',' read -ra images_array <<< "$images"
 
 # Loop through each image
 for image in "${images_array[@]}"; do
-    # Pull the image
-    docker pull $pull_registry/$image
-
-    # Retag the image
-    docker tag $pull_registry/$image $push_registry/$image
+    # Check if pull-registry is provided
+    if [ -n "$pull_registry" ]; then
+        # Pull the image
+        docker pull $pull_registry/$image
+        # Retag the image
+        docker tag $pull_registry/$image $push_registry/$image
+    else
+        # Pull the image without a registry prefix
+        docker pull $image
+        # Retag the image
+        docker tag $image $push_registry/$image
+    fi
 
     # Push the image
     docker push $push_registry/$image
